468e48af4397b8650664febd7ca445e5b2ed6a27
valeriamuccio <10616706@polimi.it>
2020-04-02 15:53:53 +0200

    Starting server

---
+++ src/main/java/it/polimi/ing/sw/psp017/controller/Server.java
@@ -0,0 +1,55 @@
+package it.polimi.ing.sw.psp017.controller;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public final static int SOCKET_PORT = 7777;
+
+
+    public static void main(String[] args)
+    {
+        Server server = new Server();
+        ServerSocket socket;
+        try {
+            socket = new ServerSocket(SOCKET_PORT);
+        } catch (IOException e) {
+            System.out.println("cannot open server socket");
+            System.exit(1);
+            return;
+        }
+
+        server.waitFirstPlayer(socket);
+
+
+        while (true) {
+            try {
+                /* accepts connections; for every connection we accept,
+                 * create a new Thread executing a ClientHandler */
+                Socket client = socket.accept();
+                ClientHandler clientHandler = new ClientHandler(client);
+                Thread thread = new Thread(clientHandler, "server_" + client.getInetAddress());
+                thread.start();
+            } catch (IOException e) {
+                System.out.println("connection dropped");
+            }
+        }
+    }
+
+    private void waitFirstPlayer(ServerSocket socket){
+
+            try {
+                /* accepts connections; for every connection we accept,
+                 * create a new Thread executing a ClientHandler */
+                Socket client = socket.accept();
+
+            }catch (IOException e) {
+                System.out.println("connection dropped");
+            }
+
+    }
+
+
+}
---
+++ src/main/java/it/polimi/ing/sw/psp017/controller/ServerState.java
@@ -0,0 +1,5 @@
+package it.polimi.ing.sw.psp017.controller;
+
+public enum ServerState {
+    WAIT_FIRST_PLAYER,CREATE_LOBBY, SELECTING_CARD ;
+}
