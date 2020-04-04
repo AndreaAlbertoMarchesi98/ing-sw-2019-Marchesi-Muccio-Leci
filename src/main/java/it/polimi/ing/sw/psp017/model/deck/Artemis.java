a5ee708de2aec648fd9019ff7631dd93ed8d8c50
aam63 <aam6353@gmail.com>
2020-04-04 16:17:38 +0200

    Artemis and Minotaur implementation, added a couple of method in Board and Vector2

--- src/main/java/it/polimi/ing/sw/psp017/model/deck/Artemis.java
+++ src/main/java/it/polimi/ing/sw/psp017/model/deck/Artemis.java
@@ -1,4 +1,50 @@
 package it.polimi.ing.sw.psp017.model.deck;
 
-public class Artemis {
+import it.polimi.ing.sw.psp017.model.Board;
+import it.polimi.ing.sw.psp017.model.Step;
+import it.polimi.ing.sw.psp017.model.Tile;
+
+public class Artemis extends BaseCard {
+
+    @Override
+    public boolean hasChoice(int stepNumber){
+        return stepNumber == 1;
+    }
+
+    @Override
+    public boolean canMove(int stepNumber, boolean isPowerActive) {
+        switch (stepNumber) {
+            case 0:
+                return true;
+            case 1:
+                return isPowerActive;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public boolean canBuild(int step, boolean isPowerActive) {
+        switch (step) {
+            case 1:
+                return !isPowerActive;
+            case 2:
+                return isPowerActive;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public boolean isValidMove(Step currentStep, Step previousStep, Board board) {
+        Tile targetTile = currentStep.getTargetTile();
+        if(currentStep.isPowerActive()){
+            if(targetTile!=previousStep.getCurrentTile())
+                return super.isValidMove(currentStep, previousStep, board);
+            else
+                return false;
+        }
+        else
+            return super.isValidMove(currentStep, previousStep, board);
+    }
 }
--- src/main/java/it/polimi/ing/sw/psp017/model/deck/Minotaur.java
+++ src/main/java/it/polimi/ing/sw/psp017/model/deck/Minotaur.java
@@ -1,4 +1,59 @@
 package it.polimi.ing.sw.psp017.model.deck;
 
-public class Minotaur {
+import it.polimi.ing.sw.psp017.model.*;
+
+public class Minotaur extends BaseCard {
+
+    @Override
+    public boolean isValidMove(Step currentStep, Step previousStep, Board board) {
+        Tile currentTile = currentStep.getCurrentTile();
+        Tile targetTile = currentStep.getTargetTile();
+        if (targetTile.getWorker() != null) {
+            if (targetTile.getWorker().getOwner() != currentTile.getWorker().getOwner())
+                return canKnockOutWorker(currentStep, board);
+            else
+                return false;
+        } else
+            return super.isValidMove(currentStep, previousStep, board);
+    }
+
+    @Override
+    public void move(Step currentStep, Step previousStep, Board board) {
+        Tile targetTile = currentStep.getTargetTile();
+        if(targetTile.getWorker()!=null)
+            knockOutWorker(currentStep, board);
+        else
+            super.move(currentStep, previousStep, board);
+    }
+
+    private Vector2d calculatePushPosition(Vector2d currentPosition, Vector2d targetPosition) {
+        Vector2d direction = Vector2d.subtractVectors(targetPosition, currentPosition);
+        return Vector2d.sumVectors(targetPosition,direction);
+    }
+
+    private boolean canKnockOutWorker(Step currentStep, Board board) {
+        Tile currentTile = currentStep.getCurrentTile();
+        Tile targetTile = currentStep.getTargetTile();
+
+        Vector2d pushPosition = calculatePushPosition(currentTile.getPosition(), targetTile.getPosition());
+
+        return Board.isInsideBounds(pushPosition) && board.isTileEmpty(board.getTile(pushPosition));
+    }
+
+    private void knockOutWorker(Step currentStep, Board board) {
+        Tile currentTile = currentStep.getCurrentTile();
+        Tile targetTile = currentStep.getTargetTile();
+        Vector2d pushPosition = calculatePushPosition(currentTile.getPosition(), targetTile.getPosition());
+        Tile pushTile = board.getTile(pushPosition);
+
+        Worker enemyWorker = targetTile.getWorker();
+        Worker worker = currentTile.getWorker();
+
+        pushTile.setWorker(enemyWorker);
+        targetTile.setWorker(worker);
+        currentTile.setWorker(null);
+
+        enemyWorker.setPosition(pushPosition);
+        worker.setPosition(currentTile.getPosition());
+    }
 }
